45d415a941f924ba4ce8c805bc4c1c9947b80303
GorbanevaEkaterina <105439408+GorbanevaEkaterina@users.noreply.github.com>
2024-07-03 14:54:53 +0400

    feat:creat card

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,13 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector(".places__list");
+const placesListTemplate = document.querySelector("#card-template").content;
+
+initialCards.forEach(function (element) {
+  const placesCard = placesListTemplate.querySelector(".card").cloneNode(true);
+  placesCard.querySelector(".card__title").textContent = element.name;
+  placesCard.querySelector(".card__image").src = element.link;
+
+  placesList.append(placesCard);
+});
